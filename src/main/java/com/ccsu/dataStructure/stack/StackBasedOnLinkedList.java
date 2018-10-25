d5b680217388dd9373cb9308be0b1ec24928865b
zhuxiaolei <524868226@qq.com>
2018-10-25 21:09:04 +0800

    基于链表的栈

---
+++ src/main/java/com/ccsu/dataStructure/stack/StackBasedOnLinkedList.java
@@ -0,0 +1,27 @@
+package com.ccsu.dataStructure.stack;
+
+/**
+ * @Author: Xiaolei Zhu
+ * @Date: 2018/10/25 17:27
+ * @Describe:
+ * @Email: mr.zhuxiaolei@gmail.com
+ */
+public class StackBasedOnLinkedList {
+
+
+    public void push(Node node) {
+
+    }
+
+    public int pop() {
+        int value = 0;
+
+
+        return value;
+    }
+
+    private static class Node{
+        private int data;
+        private Node node;
+    }
+}
