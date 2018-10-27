fc81b4a27cb6059f4d0ec7601dd3e7b6e5615d6a
zhuxiaolei <524868226@qq.com>
2018-10-27 12:48:22 +0800

    基于链表的栈

--- src/main/java/com/ccsu/dataStructure/stack/StackBasedOnLinkedList.java
+++ src/main/java/com/ccsu/dataStructure/stack/StackBasedOnLinkedList.java
@@ -7,21 +7,55 @@ package com.ccsu.dataStructure.stack;
  * @Email: mr.zhuxiaolei@gmail.com
  */
 public class StackBasedOnLinkedList {
+    private Node top = null;
 
-
-    public void push(Node node) {
-
+    /**
+     * 1. 第一个值插入
+     * 2. 之后节点插入:相当于每次都是插入到头结点
+     * @param value
+     */
+    public void push(int value) {
+        Node newNode = new Node(value, null);
+        if (top == null) {
+            top = newNode;
+        } else {
+            newNode.next = top;
+            top = newNode;
+        }
     }
 
     public int pop() {
-        int value = 0;
-
-
-        return value;
+        if (top == null) {
+            return -1;
+        }
+        top = top.next;
+        return top.data;
     }
 
-    private static class Node{
+    public void printAll() {
+        Node p = top;
+        while (p != null) {
+            System.out.print(p.data + " ");
+            p = p.next;
+        }
+    }
+
+    private static class Node {
         private int data;
-        private Node node;
+        private Node next;
+
+        public Node(int data, Node next) {
+            this.data = data;
+            this.next = next;
+        }
+    }
+
+
+    public static void main(String[] args) {
+        StackBasedOnLinkedList stackBasedOnLinkedList = new StackBasedOnLinkedList();
+        for (int i = 0; i < 5; i++) {
+            stackBasedOnLinkedList.push(i);
+        }
+        stackBasedOnLinkedList.printAll();
     }
 }
