882ba733cf1ed4d1b25c6e478e2770a90413fc17
zhuxiaolei <524868226@qq.com>
2018-11-25 21:28:06 +0800

    消息队列-队列模式的入门学习

--- src/main/java/com/ccsu/activeMQ/topic/ConsumerTest.java
+++ src/main/java/com/ccsu/activeMQ/topic/ConsumerTest.java
@@ -1,4 +1,4 @@
-package com.ccsu.activeMQ.queue;
+package com.ccsu.activeMQ.topic;
 
 import org.apache.activemq.ActiveMQConnectionFactory;
 
@@ -10,17 +10,11 @@ import javax.jms.*;
  * @author: Xiaolei Zhu
  * @Date: 2018/11/17
  * @Time: 23:25
- * Description: 消息平台-队列模式-消费者
- * 多个消费者的时候 会平分生产者产生的消息
- * 多个 Consumer 可以注册到同一个 queue 上，
- * 但一个消息只能被一个 Consumer 所接收，
- * 然后由该 Consumer 来确认消息。
- * 并且在这种情况下，Provider 对所有注册的 Consumer 以*轮询*的方式发送消息。
- *
+ * Description: 消息平台-主题模式-消费者
  */
 public class ConsumerTest {
     private static final String url = "tcp://127.0.0.1:61616";//默认端口号
-    private static final String queueName = "queue-test";
+    private static final String topicName = "queue-test";
 
     public static void main(String[] args) throws JMSException {
         //1. 创建连接工厂
@@ -32,10 +26,10 @@ public class ConsumerTest {
         //4. 创建会话
         Session session = connection.createSession(false, Session.AUTO_ACKNOWLEDGE);
         //5. 创建一个目标
-        Destination destination = session.createQueue(queueName);
+        Destination destination = session.createTopic(topicName);
         //6. 创建一个消费者
         MessageConsumer consumer = session.createConsumer(destination);
-        //7. 创建一个监听器 如下为异步接收
+        //7. 创建一个监听器
         consumer.setMessageListener(message -> {
             TextMessage textMessage = ((TextMessage) message);
             try {
@@ -44,9 +38,6 @@ public class ConsumerTest {
                 e.printStackTrace();
             }
         });
-        //7 同步接收
-        Message receive = consumer.receive();
-
 
         //8. 关闭连接
         //connection.close();
