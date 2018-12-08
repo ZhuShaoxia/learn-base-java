06e0fbb5a5f32029d9e6ac2c6578aaed35435021
dabawang <524868226@qq.com>
2018-12-08 16:14:05 +0800

    log4j 日志学习

---
+++ src/main/java/com/ccsu/log/Test.java
@@ -0,0 +1,24 @@
+package com.ccsu.log;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * Created with IntelliJ IDEA.
+ * Description:
+ * Author: Xiaolei Zhu
+ * Date: 2018-12-04
+ * Time: 16:47
+ * Email: mr.zhuxiaolei@gmail.com
+ *
+ * 日志测试
+ */
+public class Test {
+        private static final Logger logger = LoggerFactory.getLogger(Test.class);
+        public static void main(String[] args) {
+            logger.info("Method:Hello");
+            logger.info("World");
+            logger.debug("123");
+            logger.error("error");
+        }
+}
